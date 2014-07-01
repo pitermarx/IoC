6a7d7b65cc2a156d68101a203c1bbb2609f6e934
pitermarx <pedrohmarques@gmail.com>
2014-07-01 15:02:38 +0100

    Initial Commit

---
+++ Container.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace pitermarx.IoC
+{
+    public class Container
+    {
+        public readonly string Name;
+
+        private readonly Dictionary<Type, Dictionary<string, Func<object>>> getters;
+        private bool isSealed;
+
+        internal Container(string instanceName)
+        {
+            this.Name = instanceName;
+            this.getters = new Dictionary<Type, Dictionary<string, Func<object>>>();
+        }
+
+        public Container Register<T>(Func<T> getter, string getterName = "")
+            where T : class 
+        {
+            if (isSealed)
+            {
+                throw new ConstraintException("The IoC instance is sealed");
+            }
+
+            var dic = this.getters.ContainsKey(typeof(T))
+                ? this.getters[typeof(T)]
+                : (this.getters[typeof(T)] = new Dictionary<string, Func<object>>());
+            dic[getterName ?? string.Empty] = getter;
+            return this;
+        }
+
+        public T Get<T>(string getterName = "")
+        {
+            return (T)this.getters[typeof(T)][getterName ?? string.Empty]();
+        }
+
+        public void Seal()
+        {
+            this.isSealed = true;
+        }
+
+        internal void Dispose()
+        {
+            this.Seal();
+            this.getters.Clear();
+        }
+    }
+}
---
+++ IoCTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+using NUnit.Framework;
+
+namespace pitermarx.IoC
+{
+    [TestFixture]
+    public class IoCTests
+    {
+        [Test]
+        public void IoCIntegrationTests()
+        {
+            Assert.IsEmpty(Factory.Instances, "Instances starts empty");
+            
+            // if doesn't exist, create one
+            var cont = Factory.GetContainer("TestContainer");
+
+            Assert.IsNotEmpty(Factory.Instances, "TestContainer was created");
+            
+            // register two instances of the IoCTests Class. One with default name, the other with the name "2"
+            cont.Register(() => this);
+            cont.Register(() => new IoCTests(), "2");
+
+            Assert.AreSame(cont.Get<IoCTests>(), this, "Default instance is 'this'");
+            Assert.AreNotSame(cont.Get<IoCTests>("2"), this, "Named instance is not 'this'");
+            
+            Assert.Throws<KeyNotFoundException>(() => cont.Get<IoCTests>("3"), "Named instance '3' doesn't exist");
+
+            // seal container to prevent further registers
+            cont.Seal();
+            
+            Assert.Throws<ConstraintException>(() => cont.Register(() => this), "Cannot register. Container is sealed");
+            
+            // assert i can still get registered instances
+            Assert.AreSame(cont.Get<IoCTests>(), this);
+            Assert.AreNotSame(cont.Get<IoCTests>("2"), this);
+            // and the container still exists
+            Assert.IsNotEmpty(Factory.Instances);
+
+            // dispose container
+            Factory.DisposeContainer(cont);
+            
+            Assert.IsEmpty(Factory.Instances, "All the containers have been disposed");
+            Assert.Throws<KeyNotFoundException>(() => cont.Get<IoCTests>(), "This container has been disposed");
+        }
+    }
+}
